59973eca500525f6b73de554cb6399faab9568aa
hkacadej <hkacadej@gmail.com>
2025-05-30 13:02:22 +0200

    Adding stateless security management

--- src/main/java/com/henrikacadej/urlshortener/config/security/SecurityConfig.java
+++ src/main/java/com/henrikacadej/urlshortener/config/security/SecurityConfig.java
@@ -1,31 +1,53 @@
 package com.henrikacadej.urlshortener.config.security;
 
+import com.henrikacadej.urlshortener.security.filter.JwtAuthenticationFilter;
+import com.henrikacadej.urlshortener.security.oauth2.SuccessHandler;
+import com.henrikacadej.urlshortener.service.UserDetailsService;
+import com.henrikacadej.urlshortener.util.JwtUtil;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final UserDetailsService userDetailsService;
+    private final JwtUtil jwtUtil;
+
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http,JwtAuthenticationFilter filter) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/oauth2/**",
                                 "/login/**",
-                                "/auth/**"
+                                "/auth/**",
+                                "/h2-console/**"
                         ).permitAll()
                         .anyRequest().authenticated()
-                ).oauth2Login(Customizer.withDefaults());
+                ).oauth2Login(oauth -> oauth
+                        .successHandler(oAuth2LoginSuccessHandler())
+                )
+                .sessionManagement(httpSecuritySessionManagementConfigurer ->
+                        httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+    }
+
+    @Bean
+    SuccessHandler oAuth2LoginSuccessHandler() {
+        return new SuccessHandler(jwtUtil, userDetailsService);
     }
 
 }
---
+++ src/main/java/com/henrikacadej/urlshortener/util/JwtUtil.java
@@ -0,0 +1,66 @@
+package com.henrikacadej.urlshortener.util;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Component
+@Slf4j
+public class JwtUtil {
+
+
+    @Value("${spring.security.jwt.secret}")
+    private  String secret ;
+
+    @Value("${spring.security.jwt.expiration}")
+    private  long validityInMs ;
+
+    public String createToken(String email) {
+        Date now = new Date();
+        Date validity = new Date(now.getTime() + validityInMs);
+
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(now)
+                .setExpiration(validity)
+                .signWith(getKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    private SecretKey getKey() {
+        return Keys.hmacShaKeyFor(secret.getBytes());
+    }
+
+    public String extractUsername(String jwt) {
+        return extractAllClaims(jwt).getSubject();
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public Boolean validateToken(String token, UserDetails userDetails) {
+        log.info("Validating token {}", token);
+        final String username = extractUsername(token);
+        boolean valid = username.equals(userDetails.getUsername());
+        log.info("Username {} is valid", username);
+        return valid;
+    }
+
+    private Boolean isTokenExpired(String token) {
+        return extractAllClaims(token).getExpiration().before(new Date());
+    }
+}
